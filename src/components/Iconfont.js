bb86bdb1df66a27029be110f2c940cc483bbfdde
lixiaolong <243644435@qq.com>
2021-04-02 13:42:11 +0800

    feat: 升级vue3.0

--- src/components/Iconfont.js
+++
@@ -1,6 +0,0 @@
-import { createFromIconfontCN } from '@ant-design/icons-vue';
-const Iconfont = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_2241745_0qi2bfpwigto.js', // 在 iconfont.cn 上生成
-});
-
-export default Iconfont;
