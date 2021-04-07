25ce72911f1a926f26c0a7f0ee18f6ff40e80268
lixiaolong <243644435@qq.com>
2021-04-07 16:00:58 +0800

    feat: react版本添加

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from './pages/home';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/IconFont.js
@@ -0,0 +1,9 @@
+import { createFromIconfontCN } from '@ant-design/icons';
+
+const IconFont = createFromIconfontCN({
+    scriptUrl: [
+        '//at.alicdn.com/t/font_2241745_iwyjzan1ky.js'
+    ],
+});
+
+export default IconFont;
