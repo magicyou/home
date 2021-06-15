c26b88a5ec99d40314c3eb80dc4bb42aa297a9ab
lixiaolong <243644435@qq.com>
2021-06-15 18:24:41 +0800

    feat: 完善 配置文件

---
+++ env.js
@@ -0,0 +1,18 @@
+module.exports = {
+    development: {
+
+        PHONE_API: 'http://127.0.0.1:7001',
+        PORT: 7001,
+        VERSION: '0.0.1',
+        MODE: 'development',
+        NODE_ENV: 'production'
+    },
+    production: {
+
+        PHONE_API: 'https://magicyou.cn/',
+        PORT: 3000,
+        VERSION: '0.0.1',
+        MODE: 'production',
+        NODE_ENV: 'production'
+    }
+}
